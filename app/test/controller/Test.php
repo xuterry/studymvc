1a795863340ec163542d0f06a8998f4e39c9ee1c
xuterry <xuterry@126.com>
2018-10-29 07:10:14 +0800

    update 29/10

--- app/test/controller/Test.php
+++ app/test/controller/Test.php
@@ -3,6 +3,7 @@ namespace app\test\controller;
 use core\Controller;
 use core\Response;
 use core\Request;
+use http\Exchange;
 /**
 /xtw
 2018
@@ -10,6 +11,25 @@ use core\Request;
 class Test extends Controller
 {
      function index($a=''){
+         $symbol='btcusdt';
+         $period='1day';
+         $size=200;
+         $ex=new Exchange(['type'=>'Binance']);
+         $ex=new Exchange(['type'=>'Coineal']);
+         // $ex=new Exchange();
+         //dump($ex->place_order('188','0.00000662','TRXBTC','sell-limit'));exit();
+        //   dump($ex->cancel_order('75633174','trxbtc'));exit();
+            //dump($ex->get_order_state('75633174','TRXBTC'));exit();
+          dump($ex->get_orders_matchresults($symbol));exit();
+          dump($ex->get_market_depth($symbol));exit();
+          dump($ex->get_market_detail($symbol));
+          dump($ex->get_detail_merged($symbol));
+          dump($ex->get_market_trade($symbol));exit();
+          dump($ex->get_history_trade($symbol,$size));exit();
+          dump($ex->get_balance('198800'));exit();
+         dump($ex->get_market_tickers());exit();
+         dump($ex->get_history_kline($symbol,$period,$size));exit();
+         dump($ex->get_common_symbols());exit();
        // echo 'hello kitty'.$a;
          $zip=new \ZipExtension();
          $req=new Request();
